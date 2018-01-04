0b9abb7527f3e3c7a2a62b0198032f4de34fc770
anonyco <wowzeryest@gmail.com>
2018-01-03 21:36:29 -0500

    Create stringtocharcode.src.js

---
+++ stringtocharcode.src.js
@@ -0,0 +1,28 @@
+!function(){
+    "use-strict";
+    var testingArray=[0], incSize=1;
+    try {
+		// test the max chunksize String.fromCharCode can work with
+        while (incSize <= 0x10000){
+            testingArray.length = incSize*2;
+            String.fromCharCode.apply(null, testingArray);
+            incSize *= 2;
+        }
+    } catch(e) {}
+	console.log(incSize)
+    testingArray.length = 0; // clean up memory
+    window.decodeArray16 = function(Uint16ArrayCodes){
+        var res = "", i = 0, length = Uint16ArrayCodes.length;
+        while (i < length){
+            res += String.fromCharCode.apply(null, Uint16ArrayCodes.slice(i, (i+=incSize)));
+        }
+        return res;
+    }
+	window.encodeString16 = function(Uint16String){
+        var res = new Uint16Array(Uint16String.length), i = Uint16String.length;
+        while (i--){
+			res[i] = Uint16String.charCodeAt(i);
+        }
+        return res;
+    }
+}();
